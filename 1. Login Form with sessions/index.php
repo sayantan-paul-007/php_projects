2868eb04d7bac410f31b4484247de899f48c856c
Sayantan Paul <sayantan@educba.com>
2023-04-19 17:11:59 +0530

    Update 1->1.1

---
+++ 1. Login Form with sessions/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login Form</title>
+</head>
+<body>
+    <form action="" method="post">
+        <h1>LOGIN</h1>
+        <p>Enter user name and password</p>
+        <label for="username">
+            Name
+        </label>
+        <input type="text" name="username">
+        <div class="name-err"></div>
+        <label for="password">
+            Password
+        </label>
+        <input type="password" name="password">
+        <div class="pass-err"></div>
+    </form>
+</body>
+</html>
